0ef488c138ca038ecf794debb1cdce8d26c13f3e
AnkushAtCryptoSkool <cryptoskool@gmail.com>
2025-01-25 20:10:01 +0530

    loops

---
+++ loops.py
@@ -0,0 +1,19 @@
+#for loop
+for i in range(5):
+    print(i+1);
+
+# using for loops with lists / tupple / set / dictonory
+a = {1,2,3,4,5,6,7,8,9}
+for item in a:
+     print(item)
+
+
+# while loop
+i=0;
+val = 201
+while(i<10):
+     print(val)
+     if(val == 205):
+       break
+     val += 1
+     i=i+1;
