486a43236626f17e1a309fd099382a00b422b36f
AnkushAtCryptoSkool <cryptoskool@gmail.com>
2025-01-25 17:04:35 +0530

    tuples and list

---
+++ list.py
@@ -0,0 +1,14 @@
+l1 = [1,2,3,4]
+
+print(type(l1))
+l1.sort()
+print(l1)
+l1.reverse();
+print(l1)
+l1.pop(2)
+print(l1)
+l1.append(23)
+print(l1)
+l1.extend([33,54,435345,5445453])
+print(l1)
+print(l1.index(33))
---
+++ tuplesN_Methods.py
@@ -0,0 +1,3 @@
+# tuples are immutables just like strings
+t = (23,1,34,33)
+print(t)
