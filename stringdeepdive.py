f982e502ab8d04a081f584571e6cfbd65ee49b16
AnkushAtCryptoSkool <cryptoskool@gmail.com>
2025-01-25 16:54:58 +0530

    lists in python

---
+++ stringdeepdive.py
@@ -0,0 +1,18 @@
+name = 'Ankush'
+
+print(name[0:3])
+
+# write a program to take input as number from user and add 6 to it
+
+number = input("Write a number and see the magic : ")
+print(int(number) + 6);
+
+a1 = ("Ankis "
+      "is my"
+      "name")
+a2 = '''
+   I am a disco
+   Dancer to nu nu \n
+   zindgi mera gana 
+'''
+print(a1,a2)
