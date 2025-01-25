3f752fc225142cd30992d0cf45f05cdec11ee70f
AnkushAtCryptoSkool <cryptoskool@gmail.com>
2025-01-25 19:36:46 +0530

    dictionary

--- tuplesN_Methods.py
+++ tuplesN_Methods.py
@@ -1,4 +1,6 @@
 # tuples are immutables just like strings
+from pprint import pformat
+
 t = (23,1,34,33)
 print(t)
 
@@ -10,5 +12,17 @@ print(set2)
 print(set.union(set2))
 print(set.intersection(set2))
 
+#dictonary | just like maps in java
+del set
 
+dict = {}
+a = set()
+print(type(dict))
+print(a,type(a))
 
+ans = {"1" : "Harry","2" : "Ankush"}
+print(ans["2"])
+print(ans.get("1"))
+print(ans.keys())
+print(ans.values())
+print(ans.items())
