136fd923df832e3ec270622419f6d0bd4c3f46ad
AnkushAtCryptoSkool <cryptoskool@gmail.com>
2025-01-25 16:05:10 +0530

    learing python basics

---
+++ main.py
@@ -0,0 +1,26 @@
+print("Hello from Ankush")
+# comments
+# print("Hello from Ankush")
+# print("Hello from Ankush")
+# print("Hello from Ankush")
+# print("Hello from Ankush")
+# print("Hello from Ankush")
+
+# Varaibles in Python
+a = 3;
+print(a);
+a = 3.2;
+print(a);
+a = True # <- boolean charcaters | either it can be True or False
+print(a);
+
+a = "Ankush"
+print(a)
+a = None
+print(a)
+
+#typecasting
+val = "5"
+print(int(val)+ 1)
+val = 5
+print(val+1)
