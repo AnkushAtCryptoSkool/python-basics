13e25b3c4e4b44ff0cde054bb616b2696276fd08
AnkushAtCryptoSkool <cryptoskool@gmail.com>
2025-01-25 16:31:09 +0530

    learing python basics

--- operators.py
+++ operators.py
@@ -1,1 +1,47 @@
-num1 = 345;
+num1 = 10;
+num2 = 3;
+
+#arthemetic operators
+print("Addition of 2 nums : " , num1+num2);
+print("Subtraction of 2 nums : " , num1-num2);
+print("Mutiply of 2 nums : " , num1*num2);
+print("Divison of 2 nums : " , num1/num2);
+print("// of 2 nums : " , num1//num2);
+print("** of 2 nums : " , num1**num2);
+print("% of 2 nums : " , num1%num2);
+
+# Assignment operator
+a = 10;
+a *=2;
+print(a);
+a = 10;
+a /=2;
+print(a);
+a = 10;
+a -=2;
+print(a);
+a = 10;
+a +=2;
+print(a);
+a = 10;
+a //=2;
+print(a);
+a = 10;
+a **=2;
+print(a);
+a = 10;
+a %=2;
+print(a);
+
+#compraison operator
+x = 10
+y = 10
+print(x>y)
+print(x<y)
+print(x==y)
+print(x!=y)
+
+# Logical Operator
+print(x == y or x==10)
+print(x!=y and x==10)
+print(not(False))
